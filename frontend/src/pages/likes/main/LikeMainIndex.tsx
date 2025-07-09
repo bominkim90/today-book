9fd838d0408ca813200bc238a618bc0d70a63f5b
bominkim90 <bomin97@naver.com>
2025-07-10 00:40:44 +0900

    fix: (찜 목록) GET 요청 api 주소 변경

--- frontend/src/pages/likes/main/LikeMainIndex.tsx
+++ frontend/src/pages/likes/main/LikeMainIndex.tsx
@@ -4,27 +4,6 @@ import type { Book } from '../../../models/book.model';
 import BookLiked from './BookLiked';
 
 export default function LikeMainIndex() {
-  // 임시 데이터
-  // const likes = [
-  //   {
-  //     isbn13: 1,
-  //     cover: 'https://via.placeholder.com/150',
-  //     title: '책 1',
-  //     author: '작가 1',
-  //     publisher: '출판사 1',
-  //     description: '책 1 설명',
-  //   },
-  //   {
-  //     isbn13: 2,
-  //     cover: 'https://via.placeholder.com/150',
-  //     title: '책 2',
-  //     author: '작가 2',
-  //     publisher: '출판사 2',
-  //     description: '책 2 설명',
-  //   },
-  // ];
-  // const isLoading = false;
-
   const { data: likes, isLoading, isError } = useGetLikes();
 
   if (isError) return <div className="layout-main">찜 목록을 불러오는 중 오류가 발생했습니다.</div>;
