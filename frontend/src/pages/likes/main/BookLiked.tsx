fef60e404c78b09308ca2b63f367301919ee74d5
bominkim90 <bomin97@naver.com>
2025-07-10 01:17:26 +0900

    fix: (찜 목록) 컴포넌트 수정

--- frontend/src/pages/likes/main/BookLiked.tsx
+++ frontend/src/pages/likes/main/BookLiked.tsx
@@ -1,8 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import type { Book } from '../../../models/book.model';
 import ButtonLike from './ButtonLike';
-import { useGetLikes } from '../../../hooks/useLikes';
-import Loading from '../../../components/Loading';
 
 export default function BookLiked({ bookData }: { bookData: Book }) {
   const isbn13 = Number(bookData.isbn13);
@@ -12,32 +10,23 @@ export default function BookLiked({ bookData }: { bookData: Book }) {
     navigate(`/book/${isbn13}`);
   }
 
-  const { data: books, isLoading, isError } = useGetLikes();
-
-  if (isError) return <div className="layout-main">찜 목록을 불러오는 중 오류가 발생했습니다.</div>;
-  if (isLoading) return <Loading />;
-
   return (
-    <>
-      {books.map((book: Book) => (
-        <div
-          key={book.isbn13}
-          className="relative shadow-sm bg-white rounded-md"
-          onClick={(e) => {
-            e.stopPropagation();
-            goDetail();
-          }}
-        >
-          <ButtonLike isbn13={isbn13} />
-          <div className="book-container">
-            <img className="book-cover" src={book.cover} alt={book.title} />
-          </div>
-          <div className="p-sm">
-            <p className="book-title">{book.title}</p>
-            <p className="book-author">{book.author}</p>
-          </div>
-        </div>
-      ))}
-    </>
+    <div
+      key={bookData.isbn13}
+      className="relative shadow-sm bg-white rounded-md"
+      onClick={(e) => {
+        e.stopPropagation();
+        goDetail();
+      }}
+    >
+      <ButtonLike isbn13={isbn13} />
+      <div className="book-container">
+        <img className="book-cover" src={bookData.cover} alt={bookData.title} />
+      </div>
+      <div className="p-sm">
+        <p className="book-title">{bookData.title}</p>
+        <p className="book-author">{bookData.author}</p>
+      </div>
+    </div>
   );
 }
--- frontend/src/pages/likes/main/LikeMainIndex.tsx
+++ frontend/src/pages/likes/main/LikeMainIndex.tsx
@@ -1,5 +1,6 @@
 import Loading from '../../../components/Loading';
 import { useGetLikes } from '../../../hooks/useLikes';
+import Main from '../../../layout/Main';
 import type { Book } from '../../../models/book.model';
 import BookLiked from './BookLiked';
 
@@ -7,20 +8,14 @@ export default function LikeMainIndex() {
   const { data: likes, isLoading, isError } = useGetLikes();
 
   if (isError) return <div className="layout-main">찜 목록을 불러오는 중 오류가 발생했습니다.</div>;
+  if (isLoading) return <Loading />;
+  if (likes.length === 0) return <Main>찜 목록이 비어있습니다.</Main>;
 
   return (
-    <div className="layout-main">
-      {isLoading ? (
-        <Loading />
-      ) : !likes || likes.length === 0 ? (
-        <div>찜 목록이 비어있습니다.</div>
-      ) : (
-        <div className="grid grid-cols-2 gap-sm">
-          {likes.map((book: Book) => (
-            <BookLiked key={book.isbn13} bookData={book} />
-          ))}
-        </div>
-      )}
+    <div className="grid grid-cols-2 gap-sm">
+      {likes.map((book: Book) => (
+        <BookLiked key={book.isbn13} bookData={book} />
+      ))}
     </div>
   );
 }
